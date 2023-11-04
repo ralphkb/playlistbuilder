67432feb296141a1dfc2492f8bdc9ce1aa2cdde6
Ralph <143613948+ralphkb@users.noreply.github.com>
2023-11-04 19:01:58 +0200

    Finish adding the components

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./Playlist.css";
+import TrackList from "../Tracklist/Tracklist.js";
+
+const Playlist = (props) => {
+  const handleNameChange = (event) => {
+    props.onNameChange(event.target.value);
+  };
+
+  return (
+    <div className="Playlist">
+      <input onChange={handleNameChange} defaultValue={"New Playlist"} />
+      <TrackList
+        tracks={props.playlistTracks}
+        isRemoval={true}
+        onRemove={props.onRemove}
+      />
+      <button className="PlaylistSave" onClick={props.onSave}>
+        Save to spotify
+      </button>
+    </div>
+  );
+};
+
+export default Playlist;
---
+++ src/components/Track/Track.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Track.css";
+
+const Track = (props) => {
+  const addTrack = (event) => {
+    props.onAdd(props.track);
+  };
+
+  const removeTrack = (event) => {
+    props.onRemove(props.track);
+  };
+
+  const renderAddOrRemove = () => {
+    if (props.isRemoval) {
+      return (
+        <button className="TrackAction" onClick={removeTrack}>
+          -
+        </button>
+      );
+    }
+    return (
+      <button className="TrackAction" onClick={addTrack}>
+        +
+      </button>
+    );
+  };
+
+  return (
+    <div className="Track">
+      <div className="TrackInfo">
+        <img src={props.track.image} alt={props.track.name} className="TrackImage" />
+        <h3>{props.track.name}</h3>
+        <p>
+          {props.track.artist} | {props.track.album}
+        </p>
+      </div>
+      {renderAddOrRemove()}
+    </div>
+  );
+};
+
+export default Track;
