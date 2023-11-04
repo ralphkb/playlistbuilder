e16d11176f79a8c0ed9040d9efc44d9fc348b4c2
Ralph <143613948+ralphkb@users.noreply.github.com>
2023-11-04 19:24:32 +0200

    Add app js file and the css with a background

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+import Header from "./components/Header/Header.js";
+import Playlist from "./components/Playlist/Playlist.js";
+import SearchBar from "./components/SearchBar/SearchBar.js";
+import SearchResults from "./components/SearchResults/SearchResults.js";
+import Spotify from "./utils/SpotifyAPI.js";
+
+const App = () => {
+  const [searchResults, setSearchResults] = useState([]);
+  const [playlistName, setPlaylistName] = useState("New Playlist");
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+
+  const search = (term) => {
+    Spotify.search(term).then(setSearchResults);
+  };
+
+  const addTrack = (track) => {
+    if (playlistTracks.some((savedTrack) => savedTrack.id === track.id)) {
+      return;
+    }
+
+    setPlaylistTracks((prevTracks) => [...prevTracks, track]);
+  };
+
+  const removeTrack = (track) => {
+    setPlaylistTracks((prevTracks) =>
+      prevTracks.filter((currentTrack) => currentTrack.id !== track.id)
+    );
+  };
+
+  const updatePlaylistName = (name) => {
+    setPlaylistName(name);
+  };
+
+  const savePlaylist = () => {
+    const trackUris = playlistTracks.map((track) => track.uri);
+    Spotify.savePlaylist(playlistName, trackUris).then(() => {
+      setPlaylistName("New Playlist");
+      setPlaylistTracks([]);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="App">
+        <SearchBar onSearch={search} />
+        <div className="App-playlist">
+          <SearchResults searchResults={searchResults} onAdd={addTrack} />
+          <Playlist
+            playlistName={playlistName}
+            playlistTracks={playlistTracks}
+            onNameChange={updatePlaylistName}
+            onRemove={removeTrack}
+            onSave={savePlaylist}
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
