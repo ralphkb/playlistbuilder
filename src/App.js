046e9a3bce80b87c105f5ee8e8dfd952db44366a
Ralph <143613948+ralphkb@users.noreply.github.com>
2023-11-05 10:48:48 +0200

    Filter selected songs on search, add and remove actions

--- src/App.js
+++ src/App.js
@@ -17,21 +17,30 @@ const App = () => {
   }, []);
 
   const search = (term) => {
-    Spotify.search(term).then(setSearchResults);
+    Spotify.search(term).then((results) => {
+      const filteredResults = results.filter((result) => {
+        return !playlistTracks.some((playlistTrack) => playlistTrack.id === result.id);
+      });
+      setSearchResults(filteredResults);
+    });
   };
 
   const addTrack = (track) => {
     if (playlistTracks.some((savedTrack) => savedTrack.id === track.id)) {
       return;
     }
-
+  
     setPlaylistTracks((prevTracks) => [...prevTracks, track]);
+    setSearchResults((prevResults) =>
+      prevResults.filter((result) => result.id !== track.id)
+    );
   };
-
+  
   const removeTrack = (track) => {
     setPlaylistTracks((prevTracks) =>
       prevTracks.filter((currentTrack) => currentTrack.id !== track.id)
     );
+    setSearchResults((prevResults) => [track, ...prevResults]);
   };
 
   const updatePlaylistName = (name) => {
