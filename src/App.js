39b398e45408f1af35bda091c4038eecfafbe540
Ralph <143613948+ralphkb@users.noreply.github.com>
2023-11-05 11:19:12 +0200

    Add some default tracks to display

--- src/App.js
+++ src/App.js
@@ -7,6 +7,41 @@ import SearchBar from "./components/SearchBar/SearchBar.js";
 import SearchResults from "./components/SearchResults/SearchResults.js";
 import Spotify from "./utils/SpotifyAPI.js";
 
+const defaultTracks = [
+  {
+    name: "Blank Space",
+    artist: "Taylor Swift",
+    album: "1989 (Deluxe)",
+    image: "https://i.scdn.co/image/ab67616d0000b27352b2a3824413eefe9e33817a",
+    id: "1u8c2t2Cy7UBoG4ArRcF5g",
+    uri: "spotify:track:1u8c2t2Cy7UBoG4ArRcF5g"
+  },
+  {
+    name: "Into You",
+    artist: "Ariana Grande",
+    album: "Dangerous Woman",
+    image: "https://i.scdn.co/image/ab67616d0000b27333342c57a9b2c4e04c97b3f5",
+    id: "76FZM38RC8XaAjJ77CVTNe",
+    uri: "spotify:track:76FZM38RC8XaAjJ77CVTNe"
+  },
+  {
+    name: "Fearless Pt. II",
+    artist: "Lost Sky",
+    album: "Fearless Pt. II",
+    image: "https://i.scdn.co/image/ab67616d0000b273df7c14e866cf14a259563ca1",
+    id: "4y1nvncvBhdoelqPMyXxis",
+    uri: "spotify:track:4y1nvncvBhdoelqPMyXxis"
+  },
+  {
+    name: "Different World (feat. CORSAK)",
+    artist: "Alan Walker",
+    album: "Different World",
+    image: "https://i.scdn.co/image/ab67616d0000b273a108e07c661f9fc54de9c43a",
+    id: "4heFQCyiW0RUEX1Q1lb008",
+    uri: "spotify:track:4heFQCyiW0RUEX1Q1lb008"
+  }
+];
+
 const App = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [playlistName, setPlaylistName] = useState("New Playlist");
@@ -14,15 +49,26 @@ const App = () => {
 
   useEffect(() => {
     Spotify.getAccessToken();
+    const filteredDefaultTracks = defaultTracks.filter((defaultTrack) => {
+      return !playlistTracks.some((track) => track.id === defaultTrack.id);
+    });
+    setSearchResults(filteredDefaultTracks);
   }, []);
 
   const search = (term) => {
-    Spotify.search(term).then((results) => {
-      const filteredResults = results.filter((result) => {
-        return !playlistTracks.some((playlistTrack) => playlistTrack.id === result.id);
+    if (term.trim() === "") {
+      const filteredDefaultTracks = defaultTracks.filter((defaultTrack) => {
+        return !playlistTracks.some((track) => track.id === defaultTrack.id);
       });
-      setSearchResults(filteredResults);
-    });
+      setSearchResults(filteredDefaultTracks);
+    } else {
+      Spotify.search(term).then((results) => {
+        const filteredResults = results.filter((result) => {
+          return !playlistTracks.some((playlistTrack) => playlistTrack.id === result.id);
+        });
+        setSearchResults(filteredResults);
+      });
+    }
   };
 
   const addTrack = (track) => {
