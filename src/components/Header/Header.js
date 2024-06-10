0b505bd765f6ca6df00fa304f344d5cad006f280
Ralph <143613948+ralphkb@users.noreply.github.com>
2024-06-10 13:05:25 +0300

    Remove header logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,6 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="header-container">
-      <img src="./logo192.png" alt="logo" className="logo" />
       <h1 className="header-text">Playlist Builder</h1>
     </header>
   );
